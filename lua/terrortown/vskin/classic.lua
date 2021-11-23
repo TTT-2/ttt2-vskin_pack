13a762fb5b95561051e758974683e92db3eb03d5
Tim Goll <tim@timgoll.de>
2021-11-23 11:10:55 +0100

    added classic theme

---
+++ lua/terrortown/vskin/classic.lua
@@ -0,0 +1,18 @@
+vskin.RegisterVSkin("classic", {
+	colors = {
+		background = Color(56, 59, 64, 255),
+		accent = Color(105, 110, 113, 255),
+		accent_dark = Color(77, 81, 84, 255),
+		scroll = Color(217, 217, 219, 255),
+		shadow = Color(0, 0, 0, 100),
+		screen = Color(56, 59, 64, 200),
+		title_text = Color(220, 225, 230, 255)
+	},
+	params = {
+		shadow_size = 0,
+		header_height = 45,
+		collapsable_height = 30,
+		border_size = 3,
+		corner_radius = 6
+	}
+})
